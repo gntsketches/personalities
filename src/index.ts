c6c932ca97c050806fc23805b7262f92f0148ff2
Gregory Tippett <gntsketches@gmail.com>
2022-10-22 15:21:36 -0400

    event listener option

--- src/index.ts
+++ src/index.ts
@@ -16,19 +16,26 @@ const state = {
 interface IOptions {
   className?: string,
   text?: string,
+  // https://medium.com/@jeffbutsch/typescript-interface-functions-c691a108e3f1
+  onClick?(): void,
 }
 
 function build(parent: HTMLElement, el: string, options?: IOptions) {
   const element = document.createElement(el);
   if (options?.className) element.classList.add(options.className);
   if (options?.text) element.innerText = options.text;
+  if (options?.onClick) element.addEventListener('click', options.onClick);
   parent.appendChild(element)
 
   return element;
 }
 
+const listenerTest = () => {
+  console.log('listening!');
+}
+
 const main = build(root, 'div', {className: 'main'});
 const header = build(main, 'h1', {className: 'header', text: state.title});
 const options = build(main, 'div', {className: 'options'});
-const mainInput = build(options, 'input');
+const mainInput = build(options, 'input', {className: 'main-input', onClick: listenerTest});
 
